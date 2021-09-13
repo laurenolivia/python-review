88f636d17ea99ddeb31544ddac689a195e9ad1ac
Lauren <lolivia.b@gmail.com>
2021-09-13 10:14:27 -0400

    demo inheritance

--- oop/employee_class.py
+++ oop/employee_class.py
@@ -31,4 +31,54 @@ class Employee:
     def is_workday(day):
         if day.weekday() == 5 or day.weekday() == 6:
             return False
-        return True                  
+        return True
+
+
+#inheritance
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emp(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())                
+
+
+
+emp_1 = Employee('Lauren', 'Burwell', 70000)
+dev_1 = Developer('Lauren', 'Burwell', 60000, 'python')
+mgr_1 = Manager('Miranda', 'Priestly', 90000, [dev_1])
+
+
+print(emp_1.fullname())
+print(dev_1.prog_lang)
+print(mgr_1.email)
+
+mgr_1.print_emp()
+
+print(isinstance(mgr_1, Manager))
+print(issubclass(Manager, Developer))
+
+
+
+
+
