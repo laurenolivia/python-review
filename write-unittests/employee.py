c43c8bd80d06afc09232b19f7809d90ed72604de
Lauren <lolivia.b@gmail.com>
2021-09-13 09:12:51 -0400

    tracking readme, write-unittests dir

---
+++ write-unittests/employee.py
@@ -0,0 +1,30 @@
+import requests
+
+class Employee:
+    """A simple employee class"""
+
+    raise_amt = 1.05
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    def email(self):
+        return f'{self.first}.{self.last}@email.com'
+
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response!'
+
+
+
