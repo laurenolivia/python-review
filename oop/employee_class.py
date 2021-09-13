0629491810a1ce048037f494312f39529e5bba90
Lauren <lolivia.b@gmail.com>
2021-09-13 03:06:33 -0400

    add cls file

---
+++ oop/employee_class.py
@@ -0,0 +1,34 @@
+class Employee:
+    """A simple employee class"""
+
+    num_of_employees = 0
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = f'{self.first}.{self.last}@company.com'
+
+        Employee.num_of_employees += 1
+
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)  
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True                  
